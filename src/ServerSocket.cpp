604f6f9f21d4c439424f491bb51d145e6ab412fc
wanghaofu <wanghaofu@163.com>
2014-10-10 17:43:20 +0800

    x

--- src/ServerSocket.cpp
+++ src/ServerSocket.cpp
@@ -10,6 +10,9 @@
 
 ServerSocket::ServerSocket ( const int port )
 {
+    ServerSocket::clientSockets=NULL;
+    ServerSocket::serviceFlag=1;
+    ServerSocket::readWriteLock=1;
     if ( ! Socket::create() )
     {
         throw SocketException ( "Could not create server socket." );
