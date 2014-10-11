b5871bb64d7f9c3afd9d192517c7a426e9a23557
wanghaofu <wanghaofu@163.com>
2014-10-11 16:44:58 +0800

    x

--- src/ServerSocket.cpp
+++ src/ServerSocket.cpp
@@ -135,20 +135,20 @@ void* ServerSocket::processMessage(void* arg)
 
     
     //在父类Socket中定义的方法 都到整整的对象
-    *clientSocket>>"hello";
+    *clientSocket.send("hello");
 
     while(serviceFlag)
     {
         //在父类中定义的方法
-      *clientSocket<<message;
+      *clientSocket.recv(message);
         if(message=="exit")
         {
 
-            send(*clientSocket,"xxx");
+            *clientSocket.send("xxx");
             DeleteClient(clientSocket);
             break;
         }else{
-            send(*clientSocket,message);
+            *clientSocket.send(message);
             //向所有用户发送消息 //该方法暂时没有写 需要便利已连接的list端口进行逐个发送
             sendMsgToAllUsers(message);
         }
